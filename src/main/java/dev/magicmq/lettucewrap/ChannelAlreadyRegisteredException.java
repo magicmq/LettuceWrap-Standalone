6370482fd7d80c137d62b2ca23d36e1e671aeb2e
magicmq <mquindt@yahoo.com>
2021-05-10 01:43:47 -0500

    Add serialVersionUID to exception

--- src/main/java/dev/magicmq/lettucewrap/ChannelAlreadyRegisteredException.java
+++ src/main/java/dev/magicmq/lettucewrap/ChannelAlreadyRegisteredException.java
@@ -2,6 +2,8 @@ package dev.magicmq.lettucewrap;
 
 public class ChannelAlreadyRegisteredException extends RuntimeException {
 
+    private static final long serialVersionUID = 3247858659205807929L;
+
     public ChannelAlreadyRegisteredException(String message) {
         super(message);
     }
